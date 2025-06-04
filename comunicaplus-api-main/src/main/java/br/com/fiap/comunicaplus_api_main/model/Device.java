b9f10820ab3185fabe6aece91a0f799cdf5f2655
leosilper <leo10silva440@gmail.com>
2025-06-03 22:19:45 -0300

    improvements made to the connection with the database

--- comunicaplus-api-main/src/main/java/br/com/fiap/comunicaplus_api_main/model/Device.java
+++ comunicaplus-api-main/src/main/java/br/com/fiap/comunicaplus_api_main/model/Device.java
@@ -1,10 +1,20 @@
 package br.com.fiap.comunicaplus_api_main.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -14,7 +24,9 @@ import lombok.*;
 public class Device {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "device_seq")
+    @SequenceGenerator(name = "device_seq", sequenceName = "SEQ_DEVICE_ID", allocationSize = 1)
+    @Column(name = "ID_DEVICE")
     private Long id;
 
     @NotBlank(message = "O nome do dispositivo é obrigatório")
--- comunicaplus-api-main/src/main/java/br/com/fiap/comunicaplus_api_main/model/Message.java
+++ comunicaplus-api-main/src/main/java/br/com/fiap/comunicaplus_api_main/model/Message.java
@@ -1,11 +1,22 @@
 package br.com.fiap.comunicaplus_api_main.model;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -15,8 +26,11 @@ import java.time.LocalDateTime;
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "message_seq")
+    @SequenceGenerator(name = "message_seq", sequenceName = "SEQ_MESSAGE", allocationSize = 1)
+    @Column(name = "ID_MESSAGE")
     private Long id;
+
 
     @NotNull(message = "O dispositivo de origem é obrigatório")
     @ManyToOne
